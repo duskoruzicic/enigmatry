b08c636cc9339177b57941684c136ba52019cb9e
Dusko Ruzicic <ruzicic.d@gmail.com>
2022-05-25 14:37:19 +0200

    added is article validation (the vendor app)

--- Vendor.WebApi/Controllers/SupplierController.cs
+++ Vendor.WebApi/Controllers/SupplierController.cs
@@ -1,57 +1,78 @@
 ﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
 using System.Web.Http;
 using Vendor.WebApi.Models;
 using Vendor.WebApi.Repositories;
 using Vendor.WebApi.Services;
+using Vendor.WebApi.Validators;
 
 namespace Vendor.WebApi.Controllers
 {
     public class SupplierController : ApiController
     {
-
         private Logger logger;
-        private DbRepository _dbRepository;
+
 
         private SupplierService _supplierService;
+        private DbRepository _dbRepository;
 
         public SupplierController()
         {
-            _dbRepository = RepositoryFactory.CreateDbRepository();
             logger = new Logger();
             _supplierService = new SupplierService();
+            _dbRepository = RepositoryFactory.CreateDbRepository();
         }
 
-        public Article GetArtice(int id)
+        [HttpGet]
+        [Route("api/getarticle")]
+        public Article GetArticle(int id)
         {
             return _supplierService.GetArticle(id);
         }
 
-        public void BuyArticle(RealArticle article, int buyerId)
+        [HttpPost]
+        [Route("api/buyarticle")]
+        public HttpResponseMessage BuyArticle(Article article, int buyerId)
         {
             var id = article.ID;
-            if (article == null)
-            {
-                throw new Exception("Could not order article");
-            }
-
-            logger.Debug("Trying to sell article with id=" + id);
-
-            article.IsSold = true;
-            article.SoldDate = DateTime.Now;
-            article.BuyerId = buyerId;
-
             try
             {
+                if (article == null)
+                {
+                    throw new ArgumentNullException("Could not order article");
+                }
+
+                logger.Debug("Trying to sell article with id=" + id);
+
+                article.IsSold = true;
+                article.SoldDate = DateTime.Now;
+                article.BuyerId = buyerId;
+
+                List<string> errormessages = article.Validate();
+
+                if (errormessages.Any())
+                {
+                    logger.Error(string.Join(",", errormessages));
+                    return new HttpResponseMessage(HttpStatusCode.InternalServerError);
+                }
+
+
                 _dbRepository.Save(article);
                 logger.Info("Article with id=" + id + " is sold.");
+                return new HttpResponseMessage(HttpStatusCode.OK);
             }
             catch (ArgumentNullException ex)
             {
                 logger.Error("Could not save article with id=" + id);
                 throw new Exception("Could not save article with id");
             }
-            catch (Exception)
+            catch (Exception ex)
             {
+                logger.Error(ex.Message);
+                return new HttpResponseMessage(HttpStatusCode.InternalServerError);
             }
         }
     }
---
+++ Vendor.WebApi/Validators/ArticleValidator.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Vendor.WebApi.Models;
+
+namespace Vendor.WebApi.Validators
+{
+    public static class ArticleValidator
+    {
+        public static List<string> Validate(this Article article)
+        {
+            List<string> errorMessages = new List<string>();
+
+            errorMessages.Add(ValidateName(article));
+            errorMessages.Add(ValidatePrice(article));
+            errorMessages.Add(ValidateSoldDate(article));
+            errorMessages.Add(ValidateBuyerId(article));
+
+            return errorMessages;
+        }
+
+
+        private static string ValidateName(Article article)
+        {
+            return string.IsNullOrEmpty(article.Name) ? "Article name is mandatory." : string.Empty;
+        }
+
+        private static string ValidatePrice(Article article)
+        {
+            return article.Price <= 0 ? "Article price must be greater than zero." : string.Empty;
+        }
+
+        private static string ValidateSoldDate(Article article)
+        {
+            return article.SoldDate > DateTime.Now ? "Article sold date cant be in the future." : string.Empty;
+        }
+
+        private static string ValidateBuyerId(Article article)
+        {
+            return article.BuyerId <= 0 ? "BuyerId must be greater than zero." : string.Empty;
+        }
+    }
+}
